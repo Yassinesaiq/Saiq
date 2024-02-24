f8498aae5cd5fae1bfabccfe0d32c9c4cf8ee18e
Yassinesaiq <158505591+Yassinesaiq@users.noreply.github.com>
2024-02-24 13:07:06 +0100

    Update urls.py
    crée un nouveau lien parent_dashboard

--- BusManagement_App/urls.py
+++ BusManagement_App/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 from .views import user_login 
 from .views import user_logout
+from .views import parent_dashboard
 
 urlpatterns = [
     # Your app's url patterns
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('dashboard/parent/', parent_dashboard, name='parent_dashboard'),
   
 ]
 
