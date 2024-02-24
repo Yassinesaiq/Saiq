410862454daf45a914cae4004862d8ce61e3ddc5
Yassinesaiq <158505591+Yassinesaiq@users.noreply.github.com>
2024-02-24 13:20:23 +0100

    Update urls.py
    lien de méthodes ajouté ( Parent-Chauffeur-Bus)

--- BusManagement_App/urls.py
+++ BusManagement_App/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('dashboard/parent/', parent_dashboard, name='parent_dashboard'),
     path('dashboard/director/', views.director_dashboard, name='director_dashboard'),
-
-  
+    path('ajouter_parent/', views.ajouter_parent, name='ajouter_parent'),
+    path('ajouter_bus/', views.ajouter_bus, name='ajouter_bus'),
+    path('ajouter_chauffeur/', views.ajouter_chauffeur, name='ajouter_chauffeur'),
 ]
 
     # ... other patterns for BusManagement_App
